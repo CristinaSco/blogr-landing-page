01d13b953a28c0c64b270e5f79446c40b0198116
Cristina Scognamiglio <77617593+CristinaSco@users.noreply.github.com>
2021-04-02 16:46:56 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,67 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+var acc = document.getElementsByClassName("accordionHamburger");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panelHamburger = this.nextElementSibling;
+    if (panelHamburger.style.display === "block") {
+      panelHamburger.style.display = "none";
+
+    } else {
+      panelHamburger.style.display = "block";
+    }
+  });
+}
+
+var acc = document.getElementsByClassName("toggle");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panelToggle = this.nextElementSibling;
+    if (panelToggle.style.display === "block") {
+      panelToggle.style.display = "none";
+
+    } else {
+      panelToggle.style.display = "block";
+      $(".toggle").hide();
+      $(".close").show();
+    }
+  });
+}
+
+var close = document.getElementsByClassName("close");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+close[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panelToggle = this.nextElementSibling;
+    if (panelToggle.style.display === "block") {
+      panelToggle.style.display = "none";
+
+    } else {
+      panelToggle.style.display = "none";
+      $(".toggle").show();
+      $(".close").hide();
+    }
+  });
+}
